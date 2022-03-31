e584f0fe0b2396c342c20a7ef314ae52a40021d0
corgi-world <developer.msw@gmail.com>
2022-04-01 02:43:19 +0900

    cors 배포 테스트

--- src/api.js
+++ src/api.js
@@ -1,9 +1,5 @@
 // "proxy": "http://13.209.73.95:8000"
 
 export function fetchWords() {
-  return fetch("/topic", {
-    headers: {
-      accepts: "application/json",
-    },
-  }).then((response) => response.json());
+  return fetch("/topic").then((response) => response.json());
 }
